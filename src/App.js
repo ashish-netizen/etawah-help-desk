c716343aca1d380d5b789f390b3dc7ef569a7629
dk1242 <dhruval16kush@gmail.com>
2021-05-07 16:00:58 +0530

    etawah-help-desk

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Navbar } from "react-bootstrap";
+import Buttons from "./Components/Buttons";
+import Forms from "./Components/Form";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{ textAlign: "center" }}>Etawah Corona HelpDisk</h1>
+      <Buttons />
+      <Forms />
     </div>
   );
 }
---
+++ src/Components/Buttons.js
@@ -0,0 +1,164 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import oxygen from "../assets/oxygen-tank.png";
+import blood from "../assets/blood.png";
+import plasma from "../assets/blood-donation.png";
+import medicines from "../assets/vitamin.png";
+import doctor from "../assets/doctor.png";
+import beds from "../assets/hospital-bed.png";
+import vaccine from "../assets/syringe.png";
+import food from "../assets/diet.png";
+
+const Buttons = () => {
+  return (
+    <div>
+      <Container
+        style={{ marginTop: "25px", textAlign: "center", alignItems: "center" }}
+        className="shadow-sm p-3 mb-5 bg-white rounded"
+      >
+        <h2>Find Help in Etawah</h2>
+        <br />
+        <Row
+          style={{
+            alignItems: "center",
+          }}
+        >
+          <Col sm={12} md={6} lg={4} style={{ textAlign: "center" }}>
+            <a href="#">
+              <Button
+                variant="outline-danger"
+                style={{ border: "none", borderRadius: "50%", margin: "5px" }}
+              >
+                <img
+                  src={oxygen}
+                  alt="Oxygen Cylinder"
+                  width="175"
+                  height="175"
+                />
+              </Button>
+              <br />
+              <h3>Oxygen Cylinder</h3>
+            </a>
+          </Col>
+          <Col sm={12} md={6} lg={4} style={{ textAlign: "center" }}>
+            <a href="#">
+              <Button
+                variant="outline-danger"
+                style={{ border: "none", borderRadius: "50%", margin: "5px" }}
+              >
+                <img
+                  src={blood}
+                  alt="Oxygen Cylinder"
+                  width="175"
+                  height="175"
+                />
+              </Button>
+              <h3>Blood</h3>
+            </a>
+          </Col>
+          <Col sm={12} md={6} lg={4} style={{ textAlign: "center" }}>
+            <a href="#">
+              <Button
+                variant="outline-danger"
+                style={{ border: "none", borderRadius: "50%", margin: "5px" }}
+              >
+                <img
+                  src={plasma}
+                  alt="Oxygen Cylinder"
+                  width="175"
+                  height="175"
+                />
+              </Button>
+              <h3>Plasma</h3>
+            </a>
+          </Col>
+          <Col sm={12} md={6} lg={4} style={{ textAlign: "center" }}>
+            <a href="#">
+              <Button
+                variant="outline-danger"
+                style={{ border: "none", borderRadius: "50%", margin: "5px" }}
+              >
+                <img
+                  src={medicines}
+                  alt="Oxygen Cylinder"
+                  width="175"
+                  height="175"
+                />
+              </Button>
+              <h3>Medicines</h3>
+            </a>
+          </Col>
+          <Col xs={12} md={6} lg={4} style={{ textAlign: "center" }}>
+            <a href="#">
+              <div>
+                <Button
+                  variant="outline-danger"
+                  style={{ borderRadius: "50%", border: "none", margin: "5px" }}
+                >
+                  <img
+                    src={doctor}
+                    alt="Oxygen Cylinder"
+                    width="175"
+                    height="175"
+                  />
+                </Button>
+                <h3>Free Doctor Assistance</h3>
+              </div>
+            </a>
+          </Col>
+          <Col xs={12} md={6} lg={4} style={{ textAlign: "center" }}>
+            <a href="#">
+              <Button
+                variant="outline-danger"
+                style={{ borderRadius: "50%", margin: "5px", border: "none" }}
+              >
+                <img
+                  src={beds}
+                  alt="Oxygen Cylinder"
+                  width="175"
+                  height="175"
+                />
+              </Button>
+              <h3>Hospital Beds</h3>
+            </a>
+          </Col>
+          <Col style={{ textAlign: "center" }}>
+            <a href="#">
+              <Button
+                variant="outline-danger"
+                style={{ borderRadius: "50%", margin: "5px", border: "none" }}
+              >
+                <img
+                  src={vaccine}
+                  alt="Oxygen Cylinder"
+                  width="175"
+                  height="175"
+                />
+              </Button>
+              <h3>Vaccination center</h3>
+            </a>
+          </Col>
+          <Col style={{ textAlign: "center" }}>
+            <a href="#">
+              <Button
+                variant="outline-danger"
+                style={{ borderRadius: "50%", margin: "5px", border: "none" }}
+              >
+                <img
+                  src={food}
+                  alt="Oxygen Cylinder"
+                  width="175"
+                  height="175"
+                />
+              </Button>
+              <h3>Free Food Service</h3>
+            </a>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Buttons;
